06bc0d8ebaa1ea47feb8abe31f836aaa2062ff69
Jaredwg2000 <77831711+Jaredwg2000@users.noreply.github.com>
2021-01-23 18:32:55 +0000

    Adding 2D functionality
    Includes update to Kinetics.py to produce the new set of data, a new .py file to plot it and a dataset produced by the new functionality.

--- Kinetics.py
+++ Kinetics.py
@@ -74,14 +74,14 @@ class Cell:
             timeStep - advances the time of the simulation
     """
 
-    def __init__(self, reactions, x, y):
+    def __init__(self, reactions, x, y, species):
         """Initialize a reaction class.
 
         Takes the inputs from calling the class and puts them into
         local variables.
         """
         self.reactions = reactions
-
+        self.species = species
         # I put this in with foresight to the extension task.
         # I had a go at it, but ran into some issues with my way of
         # tracking species concentrations. I was trying to make a
@@ -91,32 +91,35 @@ class Cell:
         self.x = x
         self.y = y
 
-    def timeStep(self, species, step):
+    def timeStep(self, step):
         """Perform a timestep of the reaction."""
         # Loop over every reaction, find the changes for each.
         for rxn in self.reactions:
-            change = rxn.getChanges(species)
+            change = rxn.getChanges(self.species)
 
             # For a reaction, the change for any species is the change
             # for that reaction. Positive for reactants, negatvie for
             # products.
             for i in rxn.reactants:
-                for s in species:
+                for s in self.species:
                     if(s["Name"] == i):
                         s["Change"] -= change
             for i in rxn.products:
-                for s in species:
+                for s in self.species:
                     if(s["Name"] == i):
                         s["Change"] += change
 
+        for sp in self.species:
+            change = diffuseSpecies(sp, ReactionVessel, self.x, self.y, 1e-1)
+            sp["Change"] += change
         # After all changes are calculated, change the concentrations.
-        for s in species:
+        for s in self.species:
             s["Conc"] += s["Change"] * step
             if(s["Conc"] < 0):
                 s["Conc"] = 0
 
 
-def Converge(rxnVessel, species):
+def Converge(rxnVessel):
     """Find equilibrium concentrations."""
     dT = 10e-6
     timeElapsed = 0
@@ -124,7 +127,7 @@ def Converge(rxnVessel, species):
 
     while not converged:
         converged = True
-        ReactionVessel.timeStep(species, dT)
+        ReactionVessel.timeStep(dT)
         timeElapsed += dT
 
         # Work out if the concentrations have converged to eqbm.
@@ -138,35 +141,85 @@ def Converge(rxnVessel, species):
     return species
 
 
-def TimeEvolve(rxnVessel, species):
+def diffuseSpecies(species, cellArray, x, y, K):
+    """Change concentrations using a diffusion equation.
+
+    Takes the concentrations of each species in adjacent Cell objects
+    and calculates a second derivative using finite differences
+    to implement the diffusion equation (i.e. d/dt = k*d^2/dx^2)
+
+    Inputs:
+        - Species: the list of species and concentations in a cell objects
+        - cellArray: the array of all cells
+        - x: the x coordinate of the cell (self.x)
+        - y: see above
+        - K: the diffusion constant. This is hard coded here but could easily
+             be a user input.
+    """
+    change = 0
+
+    # If the cell is not on an edge; find it's neighbouring cells concentration
+    # in the x direction. If it is on an edge, use its own concentration
+    if(x-1 < 0):
+        concLeft = species["Conc"]
+    else:
+        for sp in cellArray[y*noX + (x-1)].species:
+            if(sp["Name"] == species["Name"]):
+                concLeft = sp["Conc"]
+    if(x+1 >= noX):
+        concRight = species["Conc"]
+    else:
+        for sp in cellArray[y*noX + (x+1)].species:
+            if(sp["Name"] == species["Name"]):
+                concRight = sp["Conc"]
+
+    # Apply the diffusion equation
+    change += (concLeft + concRight - 2*species["Conc"]) * K
+
+    # Repeat the above for up and down (i.e. the y coordinate)
+    if(y-1 < 0):
+        concUp = species["Conc"]
+    else:
+        for sp in cellArray[(y-1)*noX + x].species:
+            if(sp["Name"] == species["Name"]):
+                concUp = sp["Conc"]
+    if(y+1 >= noY):
+        concDown = species["Conc"]
+    else:
+        for sp in cellArray[(y+1)*noX + x].species:
+            if(sp["Name"] == species["Name"]):
+                concDown = sp["Conc"]
+
+    change += (concUp + concDown - 2*species["Conc"]) * K
+
+    return change
+
+
+def TimeEvolve(position, rxnVessel, j):
     """Find concentrations over time."""
     dT = 1e-6
-    timeElapsed = 0
-    i = 0
 
     # Instead of waiting for convergence, set a time limit.
-    while(timeElapsed < 100):
-        ReactionVessel.timeStep(species, dT)
-        timeElapsed += dT
-        # Reset the changes. Can do this immediately as we don't need them.
-        for s in species:
-            s["Change"] = 0
+    rxnVessel[position].timeStep(dT)
+    # Reset the changes. Can do this immediately as we don't need them.
+    for s in rxnVessel[position].species:
+        s["Change"] = 0
 
-        # Output something at regular intervals. Using i here instead of
-        # timeElapsed due to some floating point errors. Seemed the easiest
-        # way to get around the problem.
-        if(i % 1e6 == 0):
-            with open("Part2.dat", "a") as f:
-                f.write(str(timeElapsed) + "\t")
-                for s in species:
-                    f.write(str(s["Conc"]) + "\t")
-                f.write("\n")
-        i += 1
+    # Output something at regular intervals. Using i here instead of
+    # timeElapsed due to some floating point errors. Seemed the easiest
+    # way to get around the problem.
+    if(j % 1e5 == 0):
+        timeElapsed = dT * j
+        with open("Part2.dat", "a") as f:
+            f.write(str(timeElapsed) + "\t")
+            for s in rxnVessel[position].species:
+                f.write(str(s["Conc"]) + "\t")
+            f.write("\n")
 
 
-species = []
+speciesMaster = []
 Reactions = []
-
+ReactionVessel = []
 # Ask the user what they want to do
 print("Part 1 or 2?")
 answer = int(input())
@@ -181,7 +234,8 @@ if(answer == 1):
     prod1 = ["I"]
     react2 = ["I"]
     prod2 = ["N"]
-
+    noX = 1
+    noY = 1
     # Write the header line to the data file.
     with open("Part1.dat", "w") as f:
         f.write("# [Urea] \t [D] \t [I] \t [N] \n")
@@ -197,8 +251,8 @@ if(answer == 1):
         rate2 = [730.0*pow(e, -1.72*i/scale), 7.5e-4*pow(e, 1.2*i/scale)]
         Reactions = (Reaction(react1, prod1, rate1),
                      Reaction(react2, prod2, rate2))
-        ReactionVessel = Cell(Reactions, 0, 0)
-        species = Converge(ReactionVessel, species)
+        ReactionVessel = Cell(Reactions, 0, 0, species)
+        ReactionVessel.species = Converge(ReactionVessel)
 
         # Write the data. This is different from part 2, as we only care
         # about eqbm concentations here. Part 2 writes to the data file
@@ -221,8 +275,8 @@ elif(answer == 2):
         speciesName = input()
         print("Species " + str(i+1) + " initital concentration:")
         speciesConc = input()
-        species.append({"Name": speciesName, "Conc": float(speciesConc),
-                        "Change": 0.0})
+        speciesMaster.append({"Name": speciesName, "Conc": float(speciesConc),
+                              "Change": 0.0})
 
     # Reactions
     print("How many pairs of reactions (forward + back)?")
@@ -259,14 +313,34 @@ elif(answer == 2):
 
         Reactions.append(Reaction(reactants, products, rates))
 
-    ReactionVessel = Cell(Reactions, 0, 0)
+    print("Number of points in x direction:")
+    noX = int(input())
+    print("Number of points in y direction:")
+    noY = int(input())
+
+    for y in range(noY):
+        for x in range(noX):
+            speciesCopy = []
+            for s in speciesMaster:
+                speciesCopy.append(dict(s))
+            ReactionVessel.append(Cell(Reactions, x, y, speciesCopy))
+            # This doesn't work because the dictionaries are the same one.
+    for i in range(len(ReactionVessel)):
+        if(i != 0):
+            for s in ReactionVessel[i].species:
+                s["Conc"] = s["Conc"] / 25
 
     # Header line for the data file.
     with open("Part2.dat", "w") as f:
         f.write("# t \t")
     with open("Part2.dat", "a") as f:
-        for s in species:
+        for s in speciesMaster:
             f.write("[" + s["Name"] + "] \t")
         f.write("\n")
 
-    TimeEvolve(ReactionVessel, species)
+    timeElapsed = 0
+    j = 0
+    while(j <= 10e7):
+        for i in range(len(ReactionVessel)):
+            TimeEvolve(i, ReactionVessel, j)
+        j += 1
